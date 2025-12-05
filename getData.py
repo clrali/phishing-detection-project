4aa2e1f8a88595d37e38b14fc9aefd3f2c5c742d
Eric Yan <eric.steven.yan@gmail.com>
2025-12-05 16:43:55 -0500

    removed dead code

---
+++ getData.py
@@ -0,0 +1,106 @@
+# imports needed for web scraping
+import requests as re
+from ReadDB import URLLoader
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
+
+# imports needed for creating the structured data
+from bs4 import BeautifulSoup
+import pandas as pd
+import getFeatures as getFeatures
+
+# imports for multiprocessing requests
+import time
+import asyncio
+import aiohttp
+from multiprocessing import Pool
+
+disable_warnings(InsecureRequestWarning)
+def getResponse(url):
+    try:
+        response = re.get(url, verify=False, timeout=1)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            vector = getFeatures.create_vector(soup)
+            vector.append(url)
+            print("processed: ", url)
+            return vector
+    except Exception as e:
+        pass
+    return None
+
+def pool_get(urls):
+    resp = []
+    with Pool() as p:
+        iter = p.imap(getResponse, urls)
+        for response in iter:
+            if response != None:
+                resp.append(response)
+    return resp
+if __name__ == '__main__':
+
+    loader = URLLoader(maxURLs = 50000)
+    cleanTrainingSites, cleanTestSites = loader.getCleanURLs(numURLs = 50000, split = 1.0)
+    phishingTrainingSites, phishingTestSites = loader.getPhishingURLs(numURLs = 50000, split = 1.0)
+
+    cleanSiteData = pool_get(cleanTrainingSites)
+    phishingSiteData = pool_get(phishingTrainingSites)
+
+    columns = [
+        'has_title',
+        'has_input',
+        'has_button',
+        'has_image',
+        'has_submit',
+        'has_link',
+        'has_password',
+        'has_email_input',
+        'has_hidden_element',
+        'has_audio',
+        'has_video',
+        'number_of_inputs',
+        'number_of_buttons',
+        'number_of_images',
+        'number_of_option',
+        'number_of_list',
+        'number_of_th',
+        'number_of_tr',
+        'number_of_href',
+        'number_of_paragraph',
+        'number_of_script',
+        'length_of_title',
+        'has_h1',
+        'has_h2',
+        'has_h3',
+        'length_of_text',
+        'number_of_clickable_button',
+        'number_of_a',
+        'number_of_img',
+        'number_of_div',
+        'number_of_figure',
+        'has_footer',
+        'has_form',
+        'has_text_area',
+        'has_iframe',
+        'has_text_input',
+        'number_of_meta',
+        'has_nav',
+        'has_object',
+        'has_picture',
+        'number_of_sources',
+        'number_of_span',
+        'number_of_table',
+        'URL'
+    ]
+    
+    # phishing_df = pd.DataFrame(data=phishing_data, columns=columns)
+    legitimate_df = pd.DataFrame(data=cleanSiteData, columns=columns)
+    phishing_df = pd.DataFrame(data=phishingSiteData, columns = columns)
+    
+    # labeling 0 for legitimate and 1 for phishing
+    legitimate_df['label'] = 0
+    phishing_df['label'] = 1
+    
+    # save to csv
+    legitimate_df.to_csv("datasets/structured_legitimate_data.csv", mode='w', index=False, header=True)
+    phishing_df.to_csv("datasets/structured_phishing_data.csv", mode='w', index=False, header=True)
